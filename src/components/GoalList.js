915f17830f175dd11b542fcf5fdcc4e5c84583fd
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-09 15:25:59 -0400

    setup react app, create new component

---
+++ src/components/GoalList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './GoalList.css'
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish Course</li>
+      <li>Learn all about the course main topis</li>
+      <li>Help other students in the course</li>
+    </ul>
+  );
+};
+
+export default GoalList;
