8b8f7de1c38cf6b50f36229866814300c01b6bde
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-09 15:59:04 -0400

    use state to add new goals to list

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import GoalList from "./components/GoalList";
+import NewGoal from "./components/NewGoal";
 import "./app.css";
 
 const App = () => {
-  const courseGoals= [
+  const [courseGoals, setCourseGoals] = useState([
     {id: 'cg1', text: 'Finish Course'},
     {id: 'cg2', text: 'Learn all about the course main topis'},
     {id: 'cg3', text: 'Help other students in the course'},
-  ];
+  ]);
+
+   
+
+  const handleAddGoal = newGoal => {
+    setCourseGoals(courseGoals.concat(newGoal))
+  };
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
+      <NewGoal onAddGoal={handleAddGoal}/>
       <GoalList goals={courseGoals}/>
     </div>
   );
---
+++ src/components/NewGoal.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./NewGoal.css";
+
+const NewGoal = props => {
+  const handleAddGoal = (event) => {
+    event.preventDefault();
+
+    const newGoal = {
+        id: Math.random().toString(),
+        text: 'My New Goal'
+    }
+    
+    props.onAddGoal(newGoal);
+
+  };
+
+  return (
+    <form className="new-goal" onSubmit={handleAddGoal}>
+      <input type="text" />
+      <button type="submit">Add Goal</button>
+    </form>
+  );
+};
+
+export default NewGoal;
